8b8c5fb0a47c61b15f271eb28781679fb92bd97f
alex <aryanshakya9392@gmail.com>
2023-04-18 03:45:22 +0530

    player movement added

--- a_wet_dream/Assets/scripts/Player_Movement.cs
+++ a_wet_dream/Assets/scripts/Player_Movement.cs
@@ -4,29 +4,71 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    //player movement script
-
-
+    
     public float speed;
     public float jumpforce;
     public float move;
 
+    
+    [SerializeField] private bool isgrounded;
+    
+    public Transform groundcheck;
+    public float groundcheckradius;
+    public LayerMask whatisground;
 
-    //components
+
     public Rigidbody2D rb;
 
-    // Start is called before the first frame update
+   
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
+    
+    
+    
     void Update()
     {
         move = Input.GetAxis("Horizontal");
-
         rb.velocity = new Vector2(speed * move, rb.velocity.y);
 
+
+       
+        //groundcheck
+        isgrounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, whatisground);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            rb.velocity = Vector2.up * jumpforce;
+        }
+
+        
+
+        //flip
+        flip();
+
+        
+
+    }
+
+
+
+
+
+
+    //functions
+    void flip()
+    {
+        if (move > 0)
+        {
+            transform.localScale = new Vector3(1.43f, 1.43f, 1f);
+        }
+        else if (move < 0)
+        {
+            transform.localScale = new Vector3(-1.43f, 1.43f, 1f);
+        }
     }
 }
