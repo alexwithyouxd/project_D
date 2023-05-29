774ff1f19bc21347353f7f07db7645bf72217bd6
alex <aryanshakya9392@gmail.com>
2023-05-29 06:35:35 +0530

    ggs

--- a_wet_dream/Assets/scripts/New Folder/enemyai.cs
+++ a_wet_dream/Assets/scripts/New Folder/enemyai.cs
@@ -7,17 +7,22 @@ public class enemyai : MonoBehaviour
 
     public float speed = 5f;
     public float attackDelay = 4f;
-    public int damage = 10;
+    public int damage = 5;
 
     private Transform playerTransform;
     private Vector3 lastKnownPlayerPosition;
     private bool isWaiting;
     private float waitTimer;
 
+    public float knockbackForce = 5f;
+
     public Slider healthSlider;
+
+    private Rigidbody2D rb;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         enemys = GetComponent<enemy>();
         
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -37,12 +42,14 @@ public class enemyai : MonoBehaviour
                 transform.position = Vector2.MoveTowards(transform.position, lastKnownPlayerPosition, speed * Time.deltaTime);
 
                 
-                if (transform.position == lastKnownPlayerPosition)
+                /*if (transform.position == lastKnownPlayerPosition)
                 {
                     
                     isWaiting = true;
                     waitTimer = attackDelay;
-                }
+                }*/
+
+                
             }
             else
             {
@@ -60,6 +67,34 @@ public class enemyai : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+            // Apply knockback to the player
+            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (playerRigidbody != null)
+            {
+                Vector2 knockbackDirection = playerTransform.position - transform.position;
+                knockbackDirection.Normalize();
+
+                playerRigidbody.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+            }
+            /*isWaiting = true;
+            waitTimer = attackDelay;*/
+        }
+    }
+
+   /* private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            isWaiting = true;
+            waitTimer = attackDelay;
+        }
+    }*/
     private void Attack()
     {
      
